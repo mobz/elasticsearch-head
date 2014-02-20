2d8fa1d64b234663ccfb9e39e519e57c5fc7b009
Ben Birch <bbirch@aconex.com>
2014-02-20 11:17:14 +1100

    callback helpers for jasmine

--- test/spec/specHelper.js
+++ test/spec/specHelper.js
@@ -1,1 +1,29 @@
 // find *Spec.js files in the src directory next to the corresponding source file
+
+var test = window.test = {};
+
+test.cb = (function( jasmine ) {
+	var callbacks = [];
+
+	return {
+		use: function() {
+			callbacks = [];
+		},
+		createSpy: function( name, arg, data, context ) {
+			return jasmine.createSpy( name ).andCallFake( function() {
+				callbacks.push( { cb: arguments[ arg || 0 ], data: data, context: context } );
+			});
+		},
+		execOne: function() {
+			var exec = callbacks.shift();
+			exec.cb.apply( exec.context, exec.data );
+		},
+		execAll: function() {
+			while( callbacks.length ) {
+				this.execOne();
+			}
+		}
+	};
+
+
+})( this.jasmine );
