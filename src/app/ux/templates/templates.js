02836b3d1c2f3c7e3e924f7f964ce627e3655e6f
Ben Birch <bbirch@aconex.com>
2014-07-11 12:01:52 +1000

    byte size reported binary si units, add a count prefixed

--- src/app/ux/templates/templates.js
+++ src/app/ux/templates/templates.js
@@ -7,7 +7,7 @@
 	ut.require_template = function(f) { return f.require ? { tag: "SPAN", cls: "require", text: "*" } : null; };
 
 
-	var si_prefix = ['B','k','M', 'G', 'T', 'P', 'E', 'Y'];
+	var sib_prefix = ['B','ki','Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi', 'Yi'];
 
 	ut.byteSize_template = function(n) {
 		var i = 0;
@@ -15,7 +15,18 @@
 			i++;
 			n /= 1024;
 		}
-		return (i === 0 ? n.toString() : n.toFixed( 3 - parseInt(n,10).toString().length )) + ( si_prefix[ i ] || "..E" );
+		return (i === 0 ? n.toString() : n.toFixed( 3 - parseInt(n,10).toString().length )) + ( sib_prefix[ i ] || "..E" );
+	};
+
+	var sid_prefix = ['','k','M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
+
+	ut.count_template = function(n) {
+		var i = 0;
+		while( n >= 1000 ) {
+			i++;
+			n /= 1000;
+		}
+		return i === 0 ? n.toString() : ( n.toFixed( 3 - parseInt(n,10).toString().length ) + ( sid_prefix[ i ] || "..E" ) );
 	};
 
 })( this.app );
