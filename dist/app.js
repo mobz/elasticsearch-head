930efb976d0994b0b3adf8455813e13345c67dd5
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2014-11-19 16:18:12 +0900

    replace _nodes/plugin with _nodes/plugins

--- dist/app.js
+++ dist/app.js
@@ -4059,7 +4059,7 @@
 				{ text: i18n.text("Nav.Status"), path: "_status" },
 				{ text: i18n.text("Nav.NodeStats"), path: "_cluster/nodes/stats" },
 				{ text: i18n.text("Nav.ClusterNodes"), path: "_cluster/nodes" },
-				{ text: i18n.text("Nav.Plugins"), path: "_nodes/plugin" },
+				{ text: i18n.text("Nav.Plugins"), path: "_nodes/plugins" },
 				{ text: i18n.text("Nav.ClusterState"), path: "_cluster/state" },
 				{ text: i18n.text("Nav.ClusterHealth"), path: "_cluster/health" },
 				{ text: i18n.text("Nav.Templates"), path: "_template" }
