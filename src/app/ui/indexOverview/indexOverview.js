7ec52a0427d00d4afce6cd352bf5bdc2ee741906
philipskokoh <philipskokoh@gmail.com>
2015-11-17 15:40:37 +0800

    fix indices overview tab

--- src/app/ui/indexOverview/indexOverview.js
+++ src/app/ui/indexOverview/indexOverview.js
@@ -86,13 +86,13 @@
 			] }
 		); },
 
-		_index_template: function( index ) { return (
+		_index_template: function( index ) { console.log(index); return (
 			{ tag: "TR", children: [
 				{ tag: "TD", children: [
 					{ tag: "H3", text: index.name }
 				] },
-				{ tag: "TD", text: ut.byteSize_template( index.state.index.primary_size_in_bytes ) + "/" + ut.byteSize_template( index.state.index.size_in_bytes ) },
-				{ tag: "TD", text: ut.count_template( index.state.docs.num_docs ) }
+				{ tag: "TD", text: ut.byteSize_template( index.state.primaries.store.size_in_bytes ) + "/" + ut.byteSize_template( index.state.total.store.size_in_bytes ) },
+				{ tag: "TD", text: ut.count_template( index.state.primaries.docs.count ) }
 			] }
 		); },
 		_main_template: function() {
