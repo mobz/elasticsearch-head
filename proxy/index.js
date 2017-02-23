a7d45c087ec42775d4c4734c07e6434aa682784d
Ben Birch <bbirch@aconex.com>
2017-02-24 09:40:40 +1100

    elasticsearch proxy - set cors headers appropriate for connecting to grunt server

--- proxy/index.js
+++ proxy/index.js
@@ -1,5 +1,11 @@
 const http = require("http");
 
+const CORS_SETTINGS = {
+	origin: "http://localhost:9100",
+	methods: "GET PUT POST DELETE OPTIONS HEAD"
+}
+
+
 const recipes = {};
 recipes['http-proxy'] = require("./recipes/http_proxy.js");
 
@@ -13,11 +19,23 @@ clusters.forEach( cluster => {
 		console.log( `creating proxy ${cluster.name}` );
 
 		const proxy_instance = recipes[ cluster.recipe ]( cluster.settings );
-		http.createServer( (req, res ) => {
-			console.log( `${req.method} ${cluster.name} ${req.url}` );
-			proxy_instance( req, res );
-		} ).listen( cluster.bind );
 
+		const server = http.createServer();
+		server.on('request', (req, res ) => {
+			console.log( `${req.method} ${cluster.name} ${req.url}` );
+
+			res.setHeader("Access-Control-Allow-Origin", CORS_SETTINGS.origin );
+			res.setHeader("Access-Control-Allow-Methods", CORS_SETTINGS.methods );
+
+			if (req.method === 'OPTIONS') {
+				res.send(200);
+				res.end();
+			} else {
+				proxy_instance( req, res );
+			}
+		} );
+
+		server.listen( cluster.bind )
 		console.log( `\tlocal:  http://localhost:${cluster.bind}` );
 	}
 });
