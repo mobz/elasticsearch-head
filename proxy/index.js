eedf9b72940841f6cbe85f0c2e3bbc7c85addf8f
Ben Birch <bbirch@aconex.com>
2017-02-27 11:02:38 +1100

    elasticsearch proxy - promise based connection setup, fix OPTIONS call, add support for basic authenticated end-piont

--- proxy/index.js
+++ proxy/index.js
@@ -18,25 +18,26 @@ clusters.forEach( cluster => {
 	if( cluster.enabled ) {
 		console.log( `creating proxy ${cluster.name}` );
 
-		const proxy_instance = recipes[ cluster.recipe ]( cluster.settings );
+		recipes[ cluster.recipe ]( cluster.settings )
+			.then( function( proxy ) {
+				const server = http.createServer();
+				server.on('request', (req, res ) => {
+					console.log( `${req.method} ${cluster.name} ${req.url}` );
 
-		const server = http.createServer();
-		server.on('request', (req, res ) => {
-			console.log( `${req.method} ${cluster.name} ${req.url}` );
+					res.setHeader("Access-Control-Allow-Origin", CORS_SETTINGS.origin );
+					res.setHeader("Access-Control-Allow-Methods", CORS_SETTINGS.methods );
 
-			res.setHeader("Access-Control-Allow-Origin", CORS_SETTINGS.origin );
-			res.setHeader("Access-Control-Allow-Methods", CORS_SETTINGS.methods );
+					if (req.method === 'OPTIONS') {
+						res.writeHead(200);
+						res.end();
+					} else {
+						proxy.request( req, res );
+					}
+				} );
 
-			if (req.method === 'OPTIONS') {
-				res.send(200);
-				res.end();
-			} else {
-				proxy_instance( req, res );
-			}
-		} );
-
-		server.listen( cluster.bind )
-		console.log( `\tlocal:  http://localhost:${cluster.bind}` );
+				server.listen( cluster.bind );
+				console.log( `\tlocal:  http://localhost:${cluster.bind}` );
+			});
 	}
 });
 
--- proxy/recipes/http_proxy.js
+++ proxy/recipes/http_proxy.js
@@ -3,9 +3,28 @@ const httpProxy = require("http-proxy");
 module.exports =  function( settings ) {
 	const proxy = httpProxy.createProxy( { secure: false } );
 
-	console.log( `\tremote: ${settings.target}` );
+	proxy.on('proxyReq', function(proxyReq, req, res, options) {
+		if( settings.username ) {
+			proxyReq.setHeader( "Authorization", "Basic " + new Buffer(settings.username + ":" + settings.password).toString("base64") );
+		}
+	});
 
-	return function( req, res ) {
+	function request( req, res ) {
 		proxy.web( req, res, { target: settings.target } );
 	};
+
+	function close() {
+		proxy.close();
+	}
+
+	console.log( `\tremote: ${settings.target}` );
+
+	return new Promise( function( resolve, reject ) {
+		resolve( {
+			request: request,
+			close: close
+		} );
+	} );
+
+
 };
