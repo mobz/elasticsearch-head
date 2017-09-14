7e3b43178ced5d44e953f6a1518c4889ff0b81da
Andrés Ederra <andres.ederrafernandez@telefonica.com>
2017-09-14 18:44:46 +0200

    Fixed test pass

--- src/app/ui/clusterConnect/clusterConnect.js
+++ src/app/ui/clusterConnect/clusterConnect.js
@@ -18,7 +18,8 @@
 		_node_handler: function(data) {
 			if(data) {
 				this.prefs.set("app-base_uri", this.cluster.base_uri);
-				this.cluster.setVersion(data.version.number);
+				if(data.version && data.version.number)
+					this.cluster.setVersion(data.version.number);
 			}
 		},
 		
