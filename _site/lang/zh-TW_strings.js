eae9f434f00286cc35d0c62069eb7a1957e36f39
kewang <cpckewang@gmail.com>
2018-12-13 18:35:35 +0800

    Add Traditional Chinese translation

---
+++ _site/lang/zh-TW_strings.js
@@ -0,0 +1,177 @@
+i18n.setKeys({
+	"General.Elasticsearch": "Elasticsearch",
+	"General.LoadingAggs": "讀取聚合查詢...",
+	"General.Searching": "搜尋中...",
+	"General.Search": "搜尋",
+	"General.Help": "幫助",
+	"General.HelpGlyph": "?",
+	"General.CloseGlyph": "X",
+	"General.RefreshResults": "更新",
+	"General.ManualRefresh": "手動更新",
+	"General.RefreshQuickly": "快速更新",
+	"General.Refresh5seconds": "每5秒更新",
+	"General.Refresh1minute": "每1分鐘更新",
+	"AliasForm.AliasName": "别名",
+	"AliasForm.NewAliasForIndex": "為 {0} 建立新别名",
+	"AliasForm.DeleteAliasMessage": "輸入 ''{0}''  删除 {1}. 此操作無法恢復",
+	"AnyRequest.DisplayOptions" : "顯示選項",
+	"AnyRequest.AsGraph" : "圖形視圖",
+	"AnyRequest.AsJson" : "原始 JSON",
+	"AnyRequest.AsTable" : "表格視圖",
+	"AnyRequest.History" : "歷史記錄",
+	"AnyRequest.RepeatRequest" : "重複請求",
+	"AnyRequest.RepeatRequestSelect" : "重複周期 ",
+	"AnyRequest.Transformer" : "結果轉換器",
+	"AnyRequest.Pretty": "易讀",
+	"AnyRequest.Query" : "查詢",
+	"AnyRequest.Request": "送出",
+	"AnyRequest.Requesting": "請求中...",
+	"AnyRequest.ValidateJSON": "驗證 JSON",
+	"Browser.Title": "資料瀏覽",
+	"Browser.ResultSourcePanelTitle": "原始資料",
+	"Command.DELETE": "删除",
+	"Command.SHUTDOWN": "關閉",
+	"Command.DeleteAliasMessage": "删除别名？",
+	"ClusterOverView.IndexName": "索引名稱",
+	"ClusterOverview.NumShards": "分片數",
+	"ClusterOverview.NumReplicas": "副本數",
+	"ClusterOverview.NewIndex": "新建索引",
+	"IndexActionsMenu.Title": "動作",
+	"IndexActionsMenu.NewAlias": "新建别名...",
+	"IndexActionsMenu.Refresh": "更新",
+	"IndexActionsMenu.Flush": "Flush更新",
+	"IndexActionsMenu.Optimize": "最佳化...",
+	"IndexActionsMenu.ForceMerge": "強制合併...",
+	"IndexActionsMenu.Snapshot": "网关快照",
+	"IndexActionsMenu.Analyser": "測試分析器",
+	"IndexActionsMenu.Open": "開啟",
+	"IndexActionsMenu.Close": "關閉",
+	"IndexActionsMenu.Delete": "删除...",
+	"IndexInfoMenu.Title": "訊息",
+	"IndexInfoMenu.Status": "索引狀態",
+	"IndexInfoMenu.Metadata": "索引訊息",
+	"IndexCommand.TextToAnalyze": "文本分析",
+	"IndexCommand.ShutdownMessage": "輸入 ''{0}'' 以關閉 {1} 節點. 關閉的節點無法從此界面重新啟動",
+	"IndexOverview.PageTitle": "索引總覽",
+	"IndexSelector.NameWithDocs": "{0} ({1} 個文件)",
+	"IndexSelector.SearchIndexForDocs": "搜尋 {0} 的文件，查詢條件:",
+	"FilterBrowser.OutputType": "返回格式: {0}",
+	"FilterBrowser.OutputSize": "顯示數量: {0}",
+	"Header.ClusterHealth": "叢集健康值: {0} ({1} of {2})",
+	"Header.ClusterNotConnected": "叢集健康值: 未連接",
+	"Header.Connect": "連接",
+	"Nav.AnyRequest": "複合查詢",
+	"Nav.Browser": "資料瀏覽",
+	"Nav.ClusterHealth": "叢集健康值",
+	"Nav.ClusterState": "群集狀態",
+	"Nav.ClusterNodes": "叢集節點",
+	"Nav.Info": "訊息",
+	"Nav.NodeStats": "節點狀態",
+	"Nav.Overview": "總覽",
+	"Nav.Indices": "索引",
+	"Nav.Plugins": "套件",
+	"Nav.Status": "狀態",
+	"Nav.Templates": "樣版",
+	"Nav.StructuredQuery": "基本查詢",
+	"NodeActionsMenu.Title": "動作",
+	"NodeActionsMenu.Shutdown": "關閉節點...",
+	"NodeInfoMenu.Title": "訊息",
+	"NodeInfoMenu.ClusterNodeInfo": "叢集節點訊息",
+	"NodeInfoMenu.NodeStats": "節點狀態",
+	"NodeType.Client": "節點客户端",
+	"NodeType.Coord": "協調器",
+	"NodeType.Master": "主節點",
+	"NodeType.Tribe": "分支節點",
+	"NodeType.Worker": "工作節點",
+	"NodeType.Unassigned": "未分配",
+	"OptimizeForm.OptimizeIndex": "最佳化 {0}",
+	"OptimizeForm.MaxSegments": "最大索引段數",
+	"OptimizeForm.ExpungeDeletes": "只删除被標記為删除的",
+	"OptimizeForm.FlushAfter": "最佳化後更新",
+	"OptimizeForm.WaitForMerge": "等待合併",
+	"ForceMergeForm.ForceMergeIndex": "強制合併 {0}",
+	"ForceMergeForm.MaxSegments": "最大索引段數",
+	"ForceMergeForm.ExpungeDeletes": "只删除被標記為删除的",
+	"ForceMergeForm.FlushAfter": "強制合併後更新",
+	"Overview.PageTitle" : "叢集總覽",
+	"Output.JSON": "JSON",
+	"Output.Table": "Table",
+	"Output.CSV": "CSV",
+	"Output.ShowSource": "顯示查詢語句",
+	"Preference.SortCluster": "叢集排序",
+	"Sort.ByName": "按名稱",
+	"Sort.ByAddress": "按地址",
+	"Sort.ByType": "按類型",
+	"TableResults.Summary": "查詢 {1} 個分片中用的 {0} 個. {2} 命中. 耗時 {3} 秒",
+	"QueryFilter.AllIndices": "所有索引",
+	"QueryFilter.AnyValue": "任意",
+	"QueryFilter-Header-Indices": "索引",
+	"QueryFilter-Header-Types": "類型",
+	"QueryFilter-Header-Fields": "欄位",
+	"QueryFilter.DateRangeHint.from": "從 : {0}",
+	"QueryFilter.DateRangeHint.to": "  到 : {0}",
+	"Query.FailAndUndo": "查詢失敗. 撤銷最近的更改",
+	"StructuredQuery.ShowRawJson": "顯示原始 JSON"
+});
+
+i18n.setKeys({
+	"AnyRequest.TransformerHelp" : "\
+		<p>結果轉換器用於返回結果原始 JSON 的後續處理, 將結果轉換為更有用的格式.</p>\
+		<p>轉換器應當包含 JavaScript 函數內容. 函數的返回值將傳遞给 JSON 分析器</p>\
+		<p>Example:<br>\
+		  <code>return root.hits.hits[0];</code><br>\
+		  遍歷結果並只顯示第一個元素<br>\
+		  <code>return Object.keys(root.nodes).reduce(function(tot, node) { return tot + root.nodes[node].os.mem.used_in_bytes; }, 0);</code><br>\
+		  將返回整個叢集使用的總記憶體<br></p>\
+		<p>以下函數可以方便的處理陣列與物件<br>\
+		<ul>\
+			<li><i>Object.keys</i>(object) := array</li>\
+			<li>array.<i>forEach</i>(function(prop, index))</li>\
+			<li>array.<i>map</i>(function(prop, index)) := array</li>\
+			<li>array.<i>reduce</i>(function(accumulator, prop, index), initial_value) := final_value</li>\
+		</ul>\
+		<p>當啟用重複請求時, prev 參數將會傳遞给轉換器函數. 這將用於比較並累加圖形</p>\
+		<p>Example:<br>\
+		<code>var la = [ root.nodes[Object.keys(root.nodes)[0]].os.load_average[0] ]; return prev ? la.concat(prev) : la;</code><br>\
+		將返回第一個叢集節點最近一分鐘内的平均負載\
+		將會把結果送入圖表以產生一個負載曲線圖\
+		"
+});
+
+i18n.setKeys({
+	"AnyRequest.DisplayOptionsHelp" : "\
+		<p>原始 JSON: 將完整的查詢結果轉換為原始 JSON 格式 </p>\
+		<p>圖形視圖: 將查詢結果圖形化, 將查詢結果轉換為陣列值的形式</p>\
+		<p>表格視圖: 如果查詢是一個搜尋, 可以將搜尋結果以表格形式顯示.</p>\
+		"
+});
+
+i18n.setKeys({
+	"QueryFilter.DateRangeHelp" : "\
+		<p>Date 欄位接受日期範圍的形式查詢.</p>\
+		<p>以下格式被支援:</p>\
+		<ul>\
+			<li><b>關鍵詞 / 關鍵短語</b><br>\
+				<code>now<br> today<br> tomorrow<br> yesterday<br> last / this / next + week / month / year</code><br>\
+				搜尋關鍵字匹配的日期. <code>last year</code> 將搜尋過去全年.</li>\
+			<li><b>範圍</b><br>\
+				<code>1000 secs<br> 5mins<br> 1day<br> 2days<br> 80d<br> 9 months<br> 2yrs</code> (空格可選, 同等於多個範圍修飾詞)<br>\
+				建立一個指定時間範圍的搜尋, 將圍繞<code>现在</code> 並延伸至過去與未來時間段.</li>\
+			<li><b>DateTime 與 DateTime局部</b><br>\
+				<code>2011<br> 2011-01<br> 2011-01-18<br> 2011-01-18 12<br> 2011-01-18 12:32<br> 2011-01-18 12:32:45</code><br>\
+				指定一個特定的日期範圍. <code>2011</code>會搜尋整個 2011年, 而 <code>2011-01-18 12:32:45</code> 將只搜尋1秒範圍内</li>\
+			<li><b>Time 與 Time局部</b><br>\
+				<code>12<br> 12:32<br> 12:32:45</code><br>\
+				這些格式只搜尋當天的特定時間. <code>12:32</code> 將搜尋當天的那一分鐘</li>\
+			<li><b>日期範圍</b><br>\
+				<code>2010 -&gt; 2011<br> last week -&gt; next week<br> 2011-05 -&gt;<br> &lt; now</code><br>\
+				日期範圍是將兩個日期格式串 (日期關鍵字 / DateTime / Time) 用  &lt; 或 -&gt; (效果相同) 分隔. 如果缺少任意一端，那麼在這個方向上時間將沒有限制.</li>\
+			<li><b>偏移日期範圍</b><br>\
+				<code>2010 -> 1yr<br> 3mins < now</code>\
+				搜尋包括指定方向上偏移的日期.</li>\
+			<li><b>錨定範圍</b><br>\
+				<code>2010-05-13 05:13 <> 10m<br> now <> 1yr<br> lastweek <> 1month</code><br>\
+				類似於上面的便宜日期，在兩個方向上將錨定的日期延長</li>\
+		</ul>\
+	"
+});
