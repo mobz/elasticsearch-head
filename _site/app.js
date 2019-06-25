ff13f89693521ec7d0c1880fe098464b635650e2
philipskokoh <philipskokoh@gmail.com>
2019-06-25 21:10:48 +0800

    fix ES7.x breaking ; add match query in structured query tab

--- _site/app.js
+++ _site/app.js
@@ -595,7 +595,7 @@
 			data: function( DataSourceInterface )
 		 */
 		_getSummary: function(res) {
-			this.summary = i18n.text("TableResults.Summary", res._shards.successful, res._shards.total, res.hits.total, (res.took / 1000).toFixed(3));
+			this.summary = i18n.text("TableResults.Summary", res._shards.successful, res._shards.total, (typeof res.hits.total === 'object') ? res.hits.total.value : res.hits.total, (res.took / 1000).toFixed(3));
 		},
 		_getMeta: function(res) {
 			this.meta = { total: res.hits.total, shards: res._shards, tool: res.took };
@@ -3938,9 +3938,8 @@
 						for (var subField in obj.fields) {
 							filters.push({ path: (path[path.length - 1] !== subField) ? path.concat(subField) : path, type: obj.fields[subField].type, meta: obj.fields[subField] });
 						}
-					} else {
-						filters.push({ path: path, type: obj.type, meta: obj });
 					}
+					filters.push({ path: path, type: obj.type, meta: obj });
 				}
 			}
 			if (data[this.config.index]){
@@ -4029,7 +4028,7 @@
 			} else if(spec.type === '_all') {
 				ops = ["query_string"];
 			} else if(spec.type === 'string' || spec.type === 'text' || spec.type === 'keyword') {
-				ops = ["term", "wildcard", "prefix", "fuzzy", "range", "query_string", "text", "missing"];
+				ops = ["match", "term", "wildcard", "prefix", "fuzzy", "range", "query_string", "text", "missing"];
 			} else if(spec.type === 'long' || spec.type === 'integer' || spec.type === 'float' ||
 					spec.type === 'byte' || spec.type === 'short' || spec.type === 'double') {
 				ops = ["term", "range", "fuzzy", "query_string", "missing"];
@@ -4049,7 +4048,7 @@
 		_changeQueryOp_handler: function(jEv) {
 			var op = $(jEv.target), opv = op.val();
 			op.siblings().remove(".qual,.range,.fuzzy");
-			if(opv === 'term' || opv === 'wildcard' || opv === 'prefix' || opv === "query_string" || opv === 'text') {
+			if(opv === 'match' || opv === 'term' || opv === 'wildcard' || opv === 'prefix' || opv === "query_string" || opv === 'text') {
 				op.after({ tag: "INPUT", cls: "qual", type: "text" });
 			} else if(opv === 'range') {
 				op.after(this._range_template());
--- src/app/ui/filterBrowser/filterBrowser.js
+++ src/app/ui/filterBrowser/filterBrowser.js
@@ -35,9 +35,8 @@
 						for (var subField in obj.fields) {
 							filters.push({ path: (path[path.length - 1] !== subField) ? path.concat(subField) : path, type: obj.fields[subField].type, meta: obj.fields[subField] });
 						}
-					} else {
-						filters.push({ path: path, type: obj.type, meta: obj });
 					}
+					filters.push({ path: path, type: obj.type, meta: obj });
 				}
 			}
 			if (data[this.config.index]){
@@ -126,7 +125,7 @@
 			} else if(spec.type === '_all') {
 				ops = ["query_string"];
 			} else if(spec.type === 'string' || spec.type === 'text' || spec.type === 'keyword') {
-				ops = ["term", "wildcard", "prefix", "fuzzy", "range", "query_string", "text", "missing"];
+				ops = ["match", "term", "wildcard", "prefix", "fuzzy", "range", "query_string", "text", "missing"];
 			} else if(spec.type === 'long' || spec.type === 'integer' || spec.type === 'float' ||
 					spec.type === 'byte' || spec.type === 'short' || spec.type === 'double') {
 				ops = ["term", "range", "fuzzy", "query_string", "missing"];
@@ -146,7 +145,7 @@
 		_changeQueryOp_handler: function(jEv) {
 			var op = $(jEv.target), opv = op.val();
 			op.siblings().remove(".qual,.range,.fuzzy");
-			if(opv === 'term' || opv === 'wildcard' || opv === 'prefix' || opv === "query_string" || opv === 'text') {
+			if(opv === 'match' || opv === 'term' || opv === 'wildcard' || opv === 'prefix' || opv === "query_string" || opv === 'text') {
 				op.after({ tag: "INPUT", cls: "qual", type: "text" });
 			} else if(opv === 'range') {
 				op.after(this._range_template());
